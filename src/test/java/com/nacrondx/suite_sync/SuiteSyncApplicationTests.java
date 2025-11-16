91c99b19dc5797b05bbe1c354d598fb62a49f71f
Adrian <adi.ovi.petre@gmail.com>
2025-11-16 16:20:44 +0200

    Initial boilerplate project files

---
+++ src/test/java/com/nacrondx/suite_sync/SuiteSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nacrondx.suite_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuiteSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
